71daf5aa123a4e9fdda5aefe02d116fab5ac715b
sabts <sabrinataraschi@gmail.com>
2025-04-09 19:06:33 +0200

    javaScript almost ready

--- js/scripts.js
+++ js/scripts.js
@@ -21,8 +21,8 @@ const cardDateError = document.getElementById("card-date-error");
 const cardCvcError = document.getElementById("card-cvc-error");
 
 //Expresiones regulares: espacios(si el campo esta vacio)
-const letters = /[^a-zA-ZñÑ\s]+$/; //letras y espacios
-const numbers = /\d+/;  //numeros
+const letters = /^[A-Za-zñ\s]+$/;
+const numbers = /\d+/;
 const month = /^(0[1-9]|1[0-2])$/;
 
 //Sync funtions
@@ -71,7 +71,80 @@ const validateInputContextName = () => {
     cardNameError.textContent = 'Wrong format, letters only'; } 
   }  
 
-const validateInputContextNumber = () => {}
+const validateInputContextNumber = () => {
+    event.preventDefault();
+    
+    if (numbers.test(elementNumberInput.value)) {
+      elementNumberInput.classList.remove('invalid')
+      cardNumberError.classList.remove('show');} 
+  
+    else if (elementNumberInput.value === "")
+      {elementNumberInput.classList.add('invalid')
+      cardNumberError.classList.add('show');
+      cardNumberError.textContent = 'Can’t be blank'; }
+      else if(!/^\d{16}$/.test(elementNameInput)) //obliga que sea de 16 digitos pero no distingue entre numero y letra :(
+        {
+        elementNumberInput.classList.add('invalid');
+        cardNumberError.classList.add('show');
+        cardNumberError.textContent = 'The card number must be 16 digits long'; 
+      } 
+      else {
+      elementNumberInput.classList.add('invalid');
+      cardNumberError.classList.add('show');
+      cardNumberError.textContent = 'Wrong format, numbers only'; } 
+}
+
+const validateInputContextMonth = () => {
+  event.preventDefault();
+  if (month.test(elementMnInput.value)) {
+    elementNumberInput.classList.remove('invalid')
+    cardDateError.classList.remove('show');} 
+
+  else if (elementMnInput.value === "")
+    {elementMnInput.classList.add('invalid')
+      cardDateError.classList.add('show');
+      cardDateError.textContent = 'Can’t be blank'; }
+
+  else {
+    elementMnInput.classList.add('invalid');
+    cardDateError.classList.add('show');
+     cardDateError.textContent = 'Wrong format'; } 
+}
+
+const validateInputContextYear = () => {
+  event.preventDefault();
+  if (numbers.test(elementYyInput.value)) {
+    elementYyInput.classList.remove('invalid')
+    cardDateError.classList.remove('show');} 
+
+  else if (elementYyInput.value === "")
+    {elementYyInput.classList.add('invalid')
+      cardDateError.classList.add('show');
+      cardDateError.textContent = 'Can’t be blank'; }
+
+  else {
+    elementYyInput.classList.add('invalid');
+    cardDateError.classList.add('show');
+    cardDateError.textContent = 'Wrong format'; } 
+}
+
+const validateInputContextCvc = () => {
+  event.preventDefault();
+  if (numbers.test(elementCvcInput.value)) {
+    elementCvcInput.classList.remove('invalid')
+    cardCvcError.classList.remove('show');} 
+
+  else if (elementCvcInput.value === "")
+    {elementCvcInput.classList.add('invalid')
+      cardCvcError.classList.add('show');
+      cardCvcError.textContent = 'Can’t be blank'; }
+
+  else {
+    elementCvcInput.classList.add('invalid');
+    cardCvcError.classList.add('show');
+    cardCvcError.textContent = 'Wrong format'; } 
+}
+
 
 //Sync events
 elementNameInput.addEventListener("input", nameSyncAndChange);
@@ -82,3 +155,7 @@ elementCvcInput.addEventListener("input", cvcSyncAndChange);
 
 //Validate events
 elementForm.addEventListener("submit",validateInputContextName)
+elementForm.addEventListener("submit",validateInputContextNumber)
+elementForm.addEventListener("submit",validateInputContextMonth)
+elementForm.addEventListener("submit",validateInputContextYear)
+elementForm.addEventListener("submit",validateInputContextCvc)
